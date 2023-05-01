6b1ef6b0ebf8d8b66dc89e84e5687785bb4de596
DeveloperIndigPro <developer@indigpro.ca>
2023-05-01 15:28:41 -0500

    update NavConfig to correct spelling for route key

--- web/src/core/layouts/nav/NavConfig.tsx
+++ web/src/core/layouts/nav/NavConfig.tsx
@@ -13,7 +13,7 @@ export const PageLinks = [
     items: [
       { title: 'Who We Are', path: Routes.whoweare},
       { title: 'What We Do', path: Routes.whatWeDo},
-      { title: 'Reconcilliation', path: Routes.reconcilliation},
+      { title: 'Reconcilliation', path: Routes.reconciliation},
     ],
   },
   {
