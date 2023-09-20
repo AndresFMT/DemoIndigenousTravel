5eede73e71e1a4c559dd589af61de3065327b2ba
DeveloperIndigPro <developer@indigpro.ca>
2023-09-20 15:37:25 -0500

    remove unused dependency next-sanity-image

--- web/integrations/sanity.image.ts
+++ web/integrations/sanity.image.ts
@@ -1,8 +1,5 @@
 import { SanityImageSource } from '@sanity/image-url/lib/types/types'
-import { useNextSanityImage } from 'next-sanity-image'
 import createImageUrlBuilder from '@sanity/image-url'
-
-import client from './sanity.client'
 import { dataset, projectId } from './sanity.api'
 
 const imageBuilder = createImageUrlBuilder({ projectId, dataset })
@@ -10,17 +7,10 @@ const imageBuilder = createImageUrlBuilder({ projectId, dataset })
 export const urlForImage = (source: any) =>
   imageBuilder.image(source).auto('format').fit('max')
 
-
 export function urlFor(source:SanityImageSource) {
   const url = imageBuilder.image(source)
   return url
 }
-
-export function sanityImageProp(source:SanityImageSource) {
-  const imageProps = useNextSanityImage(client, source)
-  return imageProps
-}
-
 
 export default function sanityLoader({ src, width, quality=100 }:{src:string, width:number, quality?:number}) {
   let height;
