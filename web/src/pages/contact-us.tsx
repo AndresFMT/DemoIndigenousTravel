1175eed0555db319b7a8944c6a7126aa2a9ea4ac
DeveloperIndigPro <developer@indigpro.ca>
2023-10-26 13:57:31 -0500

    resolve errors

--- web/src/pages/contact-us.tsx
+++ web/src/pages/contact-us.tsx
@@ -1,19 +1,6 @@
-import dynamic from 'next/dynamic';
-
 import Layout from 'src/core/layouts/Layout';
 
-import Link from '@mui/material/Link';
-import Stack from '@mui/material/Stack';
-import Divider from '@mui/material/Divider';
-import Grid from '@mui/material/Unstable_Grid2';
-import Container from '@mui/material/Container';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-
-const ContactMap = dynamic(() => import('src/core/components/static-map'), { ssr: false });
-import Iconify from 'src/core/components/iconify';
 import { Page } from 'src/core/components';
-
 import { ContactForm, OfficeInfo } from 'src/sections/contact';
 
 
