3e61d5d5ce15699fb6f5a972e501801cd5220952
DeveloperIndigPro <developer@indigpro.ca>
2023-11-06 12:38:52 -0600

    add scripts

--- web/src/core/components/Page.tsx
+++ web/src/core/components/Page.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import { forwardRef } from 'react';
 // @mui
 import { Box } from '@mui/material';
@@ -17,6 +18,17 @@ const Page = forwardRef<HTMLDivElement, PageProps>(({ children, meta, title }, r
       {meta}
     </Head>
 
+      {/*<!-- Google tag (gtag.js) -->*/}
+      <Script async id="gtm-load" src="https://www.googletagmanager.com/gtag/js?id=G-Q51RT4RSMW"></Script>
+      <Script id="gtm-dataLayer-init">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-Q51RT4RSMW');
+      `}
+      </Script>
     <Box ref={ref} >
       {children}
     </Box>
