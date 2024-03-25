d394d229d60cdd8073dddd611e611f7182f1b981
Andres Trujillo <andresfelipejp@hotmail.com>
2024-03-25 14:03:25 -0500

    resolve fix sort operators

--- web/src/pages/operators/index.tsx
+++ web/src/pages/operators/index.tsx
@@ -65,6 +65,7 @@ const OperatorsPage = (props: Props) => {
           return true;
         })
         .sort((a: any, b: any) => a.name.localeCompare(b.name));
+
       setPageCount(Math.ceil(filteredOperators.length/resultsPerPage));
       setOperators(filteredOperators.slice((page-1)*resultsPerPage, page*resultsPerPage));
     }
